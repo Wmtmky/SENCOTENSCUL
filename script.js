78c27499479fddc284679ed210fb02fec5e0d770
Wmtmky <wtk2.co@gmail.com>
2023-04-14 00:02:04 -0700

    apr 13

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+var lessons;
 
 window.onload = function() {
-
+    loadPage();
 }
+
+function loadPage() {
+    let resource = location.href.split("SENCOTENSCUL/")[1];
+    if(!resource) return;
+
+    if(resource.includes("overview")) {
+        let overviewPage = resource.split("#")[1];
+        if(!overviewPage) return location.href.replace("https://wmtmky.github.io/SENCOTENSCUL");
+
+        fetch("https://wmtmky.github.io/SENCOTENSCUL/lessons.json")
+        .then(jsonFile => jsonFile.json())
+        .then(jsonObject => lessons = JSON.parse(jsonObject))
+
+        loadOverview(overviewPage);
+    }
+}
+
+function loadOverview(pageID) {
+    let overviewTitle = document.getElementById("overview-title");
+
+    overviewTitle.innerText = "Unit " + pageID.toUpperCase() + ": " + lessons[pageID].title;
+}
+
 
 function toggleInfobox() {
     let infoLightbox = document.getElementById('info-lightbox');
@@ -10,7 +34,10 @@ function toggleInfobox() {
 
 function toggleUnit(unitNum, event) {
     if(event.target?.innerText.match("exercise|subdirectory")) return;
-    location.href.replace("");
+
+    //save to local storsge
+
+    location.href.replace("https://wmtmky.github.io/SENCOTENSCUL/overview#" + unitNum);
 }
 
 
