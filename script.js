b64fe5ac36e7e863af644d3703e8e849a49d30a7
Wmtmky <wtk2.co@gmail.com>
2023-04-15 12:15:26 -0700

    2023 04 15 12 15

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
+let version = "202304151050";
+
 var lessonsJSON = 
 {
     "i":{
         "title":"The SENĆOŦEN Language",
-        "desc":"",
-        "citation":"Part A",
+        "desc":"This unit is a preface to learning the SENĆOŦEN language. It contextualizes the place of SENĆOŦEN within its neighbours, and discusses the nature of words. There are no exercises in this unit.",
+        "citation":"Part 1, Pages 2-7",
         "exerciseCount":0,
         "lessons":{
             "i.1":{
-                "desc":"",
+                "desc":"The Context of SENĆOŦEN",
                 "exerciseless":true
             },
             "i.2":{
-                "desc":"",
+                "desc":"The SENĆOŦEN Word",
                 "exerciseless":true
             }
         }
     },
     "ii":{
         "title":"The SENĆOŦEN Alphabet",
-        "desc":"",
-        "citation":"Part A",
+        "desc":"In the 1970's, SENĆOŦEN language was in danger of going extinct, as younger generations were growing up speaking English. Seeing the urgency, Dave Elliot Sr. (1910 - 1985) created a writing system so remaining fluent speakers could teach the language to younger generations. He devised it so it would be easy to learn: it would be based off English, there would be one symbol per sound, and there would be no unusual characters such as those found in the standard alphabets of other indigenous North American languages.<br>The SENĆOŦEN Alphabet:<br>A Á Ⱥ B C Ć Ȼ D E H I Í J K ₭ Ḵ Ḱ L Ƚ M N Ṉ O P Q S Ś T Ŧ Ⱦ Ṯ U W W̱ X X̱ Y",
+        "citation":"Part 2, Pages 1-17",
         "exerciseCount":0,
         "lessons":{
             "ii.1":{
@@ -31,9 +33,9 @@ var lessonsJSON =
         }
     },
     "iii":{
-        "title":"The SENĆOŦEN Alphabet",
-        "desc":"",
-        "citation":"Part A",
+        "title":"Useful Phrases",
+        "desc":"Twenty-four useful phrases that should be learned right away according to Professor Montler.",
+        "citation":"Part 1, Page 7",
         "exerciseCount":0,
         "lessons":{
             "iii.1":{
@@ -45,9 +47,9 @@ var lessonsJSON =
         }
     },
     "1":{
-        "title":"The SENĆOŦEN Alphabet",
+        "title":"The Basic Sentence",
         "desc":"",
-        "citation":"Part A",
+        "citation":"§1.1 - §1.2",
         "exerciseCount":0,
         "lessons":{
             "1.1":{
@@ -96,7 +98,7 @@ function loadOverview(pageID) {
             <div class="material-symbols-outlined" onclick="loadLearn('${lesson}')">menu_book</div>
         </div>
         <div class="lesson-btn-wrap">
-            <div class="material-symbols-outlined" onclick="loadReview('${lesson}')">exercise</div>
+            <div class="material-symbols-outlined" onclick="loadExercise('${lesson}')">exercise</div>
         </div>
         <div class="lesson-desc">${lesson.desc}</div>`;
     }
